cdc3aff68590771d5d0f3b939a3980d760fa7ecf
Billy Sterling <44212511+BillySterling@users.noreply.github.com>
2019-02-20 18:04:37 -0500

    continuing code development

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,55 @@
+//required for friends data source
+var oldFriends = require('../data/friends.js');
+
+// Routes and export
+module.exports = function(app){
+
+	// Get route to display a JSON of all possible friends
+	app.get('/api/friends', function(req, res){
+		res.json(friends);
+    });
+
+    app.post('/api/friends', function(req, res){
+        console.log(req.body);
+        var matchedFriend = {
+            name: "",
+            photo: "",
+            matchDiff: 0
+        }
+        var newFriend = req.body;
+        var newFriendScores = req.body.scores;
+        //console.log("NewFriend scores from survey: " + newFriendScores);
+
+        var newFriendTotal = 0;
+        for (var i = 0; i < newFriend.scores.length; i++) {
+                newFriendTotal += parseInt(newFriend.scores[i]);  
+            }
+        console.log("NewFriend total score from survey: " + newFriendTotal);
+
+        for (var r = 0; r < oldFriends.length; r++) {
+            var oldFriendsTotal = 0;
+            console.log("Old Friends: ", oldFriends[r].name);
+            for (var k = 0; k < oldFriends[r].scores.length; k++) {
+                //oldFriendsTotal += parseInt(oldFriends[r].scores[k]); 
+                oldFriendsTotal += Math.abs(parseInt(newFriendScores[r]) - parseInt(oldFriends[r].scores[k]));
+                if (oldFriendsTotal <= matchedFriend.matchDiff) {
+                    matchedFriend.name = oldFriends[r].name;
+                    matchedFriend.photo = oldFriends[r].photo;
+                    matchedFriend.matchDiff = oldFriendsTotal;
+                  }
+            }
+            console.log("oldFriend total: " + oldFriendsTotal);
+        };
+        console.log("matchedFriend: " + matchedFriend.name);  
+
+    });
+
+};
+
+// accept new friend input.  Add to friend array.  
+// you will already have friends data in arrays saved from previous inputs
+// first get new friends total score (do we really need this???)
+// Loop thru saved friends and see which has the least difference. 
+// the loop withing this loop will be getting score totals of all old friends
+
+
--- server.js
+++ server.js
@@ -20,14 +20,16 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('app/public'));
+
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
