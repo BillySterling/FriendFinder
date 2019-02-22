17904e764f2cd947900efe6aeb965329a991eb13
Billy Sterling <44212511+BillySterling@users.noreply.github.com>
2019-02-22 12:53:10 -0500

    testing using array reduce logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,15 +16,11 @@ module.exports = function(app){
     });
 
     app.post('/api/friends', function(req, res){
-//        console.log(req.body);
-        debugger;
         var matchedFriend = {
             name: "",
             photo: "",
-            matchDiff: 40
         };
-        var newFriend = req.body;
-        var newFriendScores = req.body.scores;
+        var matchDiff = 1000;
         //console.log("NewFriend scores from survey: " + newFriendScores);
 
         // debugging code
@@ -34,10 +30,13 @@ module.exports = function(app){
         //    }
         //console.log("NewFriend total score from survey: " + newFriendTotal);
 
+/*
+
         for (var r = 0; r < oldFriends.length; r++) {
             var oldFriendsTotal = 0;
             //console.log("Old Friends: ", oldFriends[r].name);
             for (var k = 0; k < oldFriends[r].scores.length; k++) {
+                debugger;
                 oldFriendsTotal += Math.abs(parseInt(newFriendScores[r]) - parseInt(oldFriends[r].scores[k]));
                 if (oldFriendsTotal <= matchedFriend.matchDiff) {
                     debugger;
@@ -54,5 +53,36 @@ module.exports = function(app){
         oldFriends.push(newFriend);
         res.json(matchedFriend);
         console.log("UPDATED: ", newFriend);
+    });
+*/
+        for (var r = 0; r < oldFriends.length; r++) {
+            var matchArr = [];
+
+            for (var k = 0; k < oldFriends[r].scores.length; k++) {
+                debugger;
+                var testVal = Math.abs((oldFriends[r].scores[k]) - (req.body.scores[k])); //debugging
+                // array to keep score calculations between input friend data and stored friend data
+                // use Math.abs to keep positive value
+                matchArr.push(Math.abs((oldFriends[r].scores[k]) - (req.body.scores[k])));
+                }
+                debugger;
+
+                var bestScore = matchArr.reduce((acc, val) => acc + val);
+
+                if (bestScore < matchDiff) {
+                    debugger;
+                    matchedFriend.name = oldFriends[r].name;
+                    matchedFriend.photo = oldFriends[r].photo;
+                    matchDiff = bestScore;
+                    }
+            }
+            console.log("matched Friend: " + matchedFriend.name);  
+            debugger;
+            // save the new friends data
+            console.log("UPDATED: ", req.body);
+            oldFriends.push(req.body);
+            // respond to user with best match
+            console.log("BEST MATCH: ", matchedFriend);
+            res.json(matchedFriend);
     });
 };
