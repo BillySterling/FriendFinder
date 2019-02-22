397e424a0ed07280737c390864d8aee036ba5140
Billy Sterling <44212511+BillySterling@users.noreply.github.com>
2019-02-22 14:51:08 -0500

    cleaning up code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,3 @@
-// accept new friend input.  Add to friend array.  
-// you will already have friends data in arrays saved from previous inputs
-// first get new friends total score (do we really need this???)
-// Loop thru saved friends and see which has the least difference. 
-// the loop withing this loop will be getting score totals of all old friends
-
 //required for friends data source
 var oldFriends = require('../data/friends');
 
@@ -21,54 +15,19 @@ module.exports = function(app){
             photo: "",
         };
         var matchDiff = 1000;
-        //console.log("NewFriend scores from survey: " + newFriendScores);
-
-        // debugging code
-        //var newFriendTotal = 0;
-        //for (var i = 0; i < newFriend.scores.length; i++) {
-        //        newFriendTotal += parseInt(newFriend.scores[i]);  
-        //    }
-        //console.log("NewFriend total score from survey: " + newFriendTotal);
-
-/*
-
         for (var r = 0; r < oldFriends.length; r++) {
-            var oldFriendsTotal = 0;
-            //console.log("Old Friends: ", oldFriends[r].name);
-            for (var k = 0; k < oldFriends[r].scores.length; k++) {
-                debugger;
-                oldFriendsTotal += Math.abs(parseInt(newFriendScores[r]) - parseInt(oldFriends[r].scores[k]));
-                if (oldFriendsTotal <= matchedFriend.matchDiff) {
-                    debugger;
-                    matchedFriend.name = oldFriends[r].name;
-                    matchedFriend.photo = oldFriends[r].photo;
-                    matchedFriend.matchDiff = oldFriendsTotal;
-                  }
-            }
-            console.log("oldFriend total: " + oldFriendsTotal);
-        }
-        console.log("matched Friend: " + matchedFriend.name);  
-        debugger;
-        // push (save) the new friends data
-        oldFriends.push(newFriend);
-        res.json(matchedFriend);
-        console.log("UPDATED: ", newFriend);
-    });
-*/
-        for (var r = 0; r < oldFriends.length; r++) {
+            // array to keep score calculations between input friend data and stored friend data
             var matchArr = [];
-
+            // loop thru the scores; compare the difference between current user's scores against those from other users, question by question.
             for (var k = 0; k < oldFriends[r].scores.length; k++) {
                 debugger;
-                var testVal = Math.abs((oldFriends[r].scores[k]) - (req.body.scores[k])); //debugging
-                // array to keep score calculations between input friend data and stored friend data
-                // use Math.abs to keep positive value
+                // Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
                 matchArr.push(Math.abs((oldFriends[r].scores[k]) - (req.body.scores[k])));
                 }
                 debugger;
-
                 var bestScore = matchArr.reduce((acc, val) => acc + val);
 
+                //The closest match will be the user with the least amount of difference.
                 if (bestScore < matchDiff) {
                     debugger;
                     matchedFriend.name = oldFriends[r].name;
